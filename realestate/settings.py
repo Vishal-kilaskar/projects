8f126e222f8f94d4ab4ca7d387e5bf7220f87912
vishal-kilaskar <vishaalv2012@gmail.com>
2019-11-17 20:33:35 +0530

    WIP

--- realestate/settings.py
+++ realestate/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'pages',
+    'pages.apps.PagesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -120,6 +120,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_DIRS = [
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'realestate/static')
 ]
