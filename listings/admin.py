af14fbb109d5b273235c48635410d7cd57e3d63c
vishal-kilaskar <vishaalv2012@gmail.com>
2020-08-09 23:27:35 +0530

    admin section customization

--- listings/admin.py
+++ listings/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
 from .models import Listing
 # Register your models here.
-admin.site.register(Listing)
+
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'is_published', 'price', 'list_date', 'realtors')
+    list_display_links = ('id', 'title')
+    list_filter = ['realtors']
+    list_editable = ['is_published']
+    search_fields = ('title', 'description', 'realtors__name', 'address', 'city', 'state', 'price')
+    list_per_page = 25
+
+
+admin.site.register(Listing, ListingAdmin)
