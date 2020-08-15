e0d85b03214c1d252d2e677ba812f79bace3e503
vishal-kilaskar <vishaalv2012@gmail.com>
2020-08-15 22:32:25 +0530

    added searching feature

--- listings/views.py
+++ listings/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Listing
+from .choices import *
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 
@@ -15,10 +16,39 @@ def index(request):
 
 
 def listing(request, listing_id):
-    listing = get_object_or_404(Listing, pk=listing_id)
-    context = {'listing': listing}
+    single_listing = get_object_or_404(Listing, pk=listing_id)
+    context = {'listing': single_listing}
     return render(request, 'listings/listing.html', context)
 
 
 def search(request):
-    return render(request, 'listings/search.html')
+    search_listing = Listing.objects.order_by('-list_date')
+
+    if 'keywords' in request.GET:
+        keywords = request.GET.get('keywords')
+        if keywords:
+            search_listing = search_listing.filter(description__icontains=keywords)
+    if 'city' in request.GET:
+        city = request.GET.get('city')
+        if city:
+            search_listing = search_listing.filter(city__iexact=city)
+    if 'state' in request.GET:
+        state = request.GET.get('state')
+        if state:
+            search_listing = search_listing.filter(state__iexact=state)
+    if 'bedrooms' in request.GET:
+        bedrooms = request.GET.get('bedrooms')
+        if bedrooms:
+            search_listing = search_listing.filter(bedrooms__lte=bedrooms)
+    if 'price' in request.GET:
+        price = request.GET.get('price')
+        if price:
+            search_listing = search_listing.filter(price__lte=price)
+    context = {
+        'bedroom_choices': bedroom_choices,
+        'state_choices': state_choices,
+        'price_choices': price_choices,
+        'listings': search_listing,
+        'values': request.GET
+    }
+    return render(request, 'listings/search.html', context)
